94ed2d9ac6c5022b3c654df73f031cd07c6be435
OlleLarsson <olle.larsson@elastisys.com>
2021-01-05 07:17:09 +0100

    tests: Remove prometheus-wc endpoint test

--- pipeline/test/services/workload-cluster/testEndpoints.sh
+++ pipeline/test/services/workload-cluster/testEndpoints.sh
@@ -14,7 +14,6 @@ enable_ck8sdash=$(yq r -e "${CONFIG_FILE}" 'ck8sdash.enabled')
 enable_user_alertmanager_ingress=$(yq r -e "${CONFIG_FILE}" 'user.alertmanager.ingress.enabled')
 enable_user_alertmanager=$(yq r -e "${CONFIG_FILE}" 'user.alertmanager.enabled')
 
-testEndpoint Prometheus-wc "https://prometheus.${base_domain}/"
 if [[ "${enable_user_alertmanager_ingress}" == "true" && "${enable_user_alertmanager}" == "true" ]]
 then
     testEndpoint Alertmanager-user "https://alertmanager.${base_domain}/"
@@ -29,7 +28,6 @@ echo
 echo "Testing endpoints protection"
 echo "============================="
 
-testEndpointProtected Prometheus-wc "https://prometheus.${base_domain}/" 401
 if [[ "${enable_user_alertmanager_ingress}" == "true" && "${enable_user_alertmanager}" == "true" ]]
 then
     testEndpointProtected Alertmanager-user "https://alertmanager.${base_domain}/" 401
