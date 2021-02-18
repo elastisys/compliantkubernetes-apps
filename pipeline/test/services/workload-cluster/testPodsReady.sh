0fe93469f3ba653d74664b682be6ee1cb36e5537
OlleLarsson <olle.larsson@elastisys.com>
2021-02-18 07:22:16 +0100

    tests: Fix and update wc pod tests
    Test nfs-client-provisioner only if it is enable instead of looking at
    what cloud proider is used. Also added test for local volume
    provisioner.

--- pipeline/test/services/workload-cluster/testPodsReady.sh
+++ pipeline/test/services/workload-cluster/testPodsReady.sh
@@ -4,12 +4,13 @@ INNER_SCRIPTS_PATH="$(dirname "$(readlink -f "${BASH_SOURCE[0]}")")"
 # shellcheck disable=SC1090
 source "${INNER_SCRIPTS_PATH}/../funcs.sh"
 
-cloud_provider=$(yq r -e "${CONFIG_FILE}" 'global.cloudProvider')
 enable_falco_alerts=$(yq r -e "${CONFIG_FILE}" 'falco.alerts.enabled')
 enable_falco=$(yq r -e "${CONFIG_FILE}" 'falco.enabled')
 enable_opa=$(yq r -e "${CONFIG_FILE}" 'opa.enabled')
 enable_user_alertmanager=$(yq r -e "${CONFIG_FILE}" 'user.alertmanager.enabled')
 enable_velero=$(yq r -e "${CONFIG_FILE}" 'velero.enabled')
+enable_local_pv_provisioner=$(yq r -e "${CONFIG_FILE}" 'storageClasses.local.enabled')
+enable_nfs_provisioner=$(yq r -e "${CONFIG_FILE}" 'storageClasses.nfs.enabled')
 
 echo
 echo
@@ -27,24 +28,23 @@ deployments=(
     "monitoring kube-prometheus-stack-operator"
     "monitoring kube-prometheus-stack-kube-state-metrics"
 )
-if [ "$cloud_provider" == "exoscale" ]; then
+if "${enable_nfs_provisioner}"; then
     deployments+=("kube-system nfs-client-provisioner")
 fi
-if [ "$enable_opa" == true ]; then
+if "{$enable_opa}"; then
     deployments+=("gatekeeper-system gatekeeper-controller-manager")
 fi
-if [ "$enable_falco_alerts" == true ]; then
+if "${enable_falco_alerts}"; then
     deployments+=("falco falcosidekick")
 fi
-if [ "$enable_velero" == true ]; then
+if "${enable_velero}"; then
     deployments+=("velero velero")
 fi
 
 resourceKind="Deployment"
 # Get json data in a smaller dataset
 simpleData="$(getStatus $resourceKind)"
-for deployment in "${deployments[@]}"
-do
+for deployment in "${deployments[@]}"; do
     read -r -a arr <<< "$deployment"
     namespace="${arr[0]}"
     name="${arr[1]}"
@@ -65,18 +65,20 @@ daemonsets=(
     "monitoring kube-prometheus-stack-prometheus-node-exporter"
     "velero restic"
 )
-if [ "$enable_falco" == true ]; then
+if "${enable_falco}"; then
     daemonsets+=("falco falco")
 fi
-if [ "$enable_velero" == true ]; then
+if "${enable_velero}"; then
     daemonsets+=("velero restic")
+fi
+if "${enable_local_pv_provisioner}"; then
+  daemonsets+=("kube-system local-volume-provisioner")
 fi
 
 resourceKind="DaemonSet"
 # Get json data in a smaller dataset
 simpleData="$(getStatus $resourceKind)"
-for daemonset in "${daemonsets[@]}"
-do
+for daemonset in "${daemonsets[@]}"; do
     read -r -a arr <<< "$daemonset"
     namespace="${arr[0]}"
     name="${arr[1]}"
@@ -92,16 +94,14 @@ statefulsets=(
     "monitoring prometheus-kube-prometheus-stack-prometheus"
 )
 
-if [[ $enable_user_alertmanager == "true" ]]
-then
+if "${enable_user_alertmanager}"; then
     statefulsets+=("monitoring alertmanager-alertmanager")
 fi
 
 resourceKind="StatefulSet"
 # Get json data in a smaller dataset
 simpleData="$(getStatus $resourceKind)"
-for statefulset in "${statefulsets[@]}"
-do
+for statefulset in "${statefulsets[@]}"; do
     read -r -a arr <<< "$statefulset"
     namespace="${arr[0]}"
     name="${arr[1]}"
