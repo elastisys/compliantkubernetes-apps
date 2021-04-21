767d7d898a34afcff0ffe1c97af525a5be922a1d
Lennart Jern <lennart.jern@elastisys.com>
2021-04-21 15:48:46 +0300

    bin: Update script for generating s3-config
    Not all providers work with some of the parameters, and others don't
    need all parameters.

--- scripts/S3/generate-s3cfg.sh
+++ scripts/S3/generate-s3cfg.sh
@@ -1,11 +1,20 @@
 #!/bin/bash
 
+set -eu -o pipefail
+
 # Simple script for generating a sample s3cfg for a couple of known
 # and tested S3 providers.
 
 function usage() {
     echo "Usage:" 1>&2
-    echo "  $0 aws|exoscale|safespring|citycloud access_key secret_key host_base region" 1>&2
+    echo "  $0 {aws|exoscale|safespring|citycloud} {access_key} {secret_key} {host_base} [region]" 1>&2
+    echo "  host_base - the host (and port if other than default) of the service" 1>&2
+    echo "  region - the location where the buckets should be stored. This is ignored for exoscale, safespring and citycloud." 1>&2
+    echo "Examples:" 1>&2
+    echo "  $0 aws abc 123 s3.amazonaws.com eu-north-1" 1>&2
+    echo "  $0 exoscale abc 123 sos-ch-gva-2.exo.io" 1>&2
+    echo "  $0 safespring abc 123 s3.sto1.safedc.net" 1>&2
+    echo "  $0 cityloud abc 123 s3-kna1.citycloud.com:8080" 1>&2
     exit 1
 }
 
@@ -14,17 +23,19 @@ function usage() {
 cat <<EOF
 access_key = $2
 secret_key = $3
-host_base = $4
+EOF
+if [ "$1" = "aws" ]; then
+cat <<EOF
 bucket_location = $5
 EOF
-# Providers that support virtual-hosted-style access to buckets.
-if [ "$1" = "aws" ] || [ "$1" = "exoscale" ]; then
+elif [ "$1" = "exoscale" ]; then
 cat <<EOF
+host_base = $4
 host_bucket = %(bucket)s.$4
 EOF
-# Providers that only support path-style access to buckets.
 elif [ "$1" = "safespring" ] || [ "$1" = "citycloud" ]; then
 cat <<EOF
+host_base = $4
 host_bucket = $4
 EOF
 else
