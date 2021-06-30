2cac315d8aa3537467f01ea1ebf1b641d87179b3
robinelastisys <robin.ramquist@elastisys.com>
2021-06-30 10:44:21 +0200

    apps sc & wc: release 0.17 fixes

--- migration/v0.16.x-v0.17.x/migrate-openid.sh
+++ migration/v0.16.x-v0.17.x/migrate-openid.sh
@@ -9,6 +9,9 @@ set -euo pipefail
 # Restart the master pods so that they mount the latest version of the secret after apply
 "${here}/../../bin/ck8s" ops kubectl sc delete pod -n elastic-system -l role=master
 
+# Wait for master pod to be ready before updating the config
+"${here}/../../bin/ck8s" ops kubectl sc wait --for=condition=ready pod  -n elastic-system opendistro-es-master-0
+sleep 10
 # Make the script executable
 "${here}/../../bin/ck8s" ops kubectl sc -n elastic-system exec opendistro-es-master-0 -- chmod +x ./plugins/opendistro_security/tools/securityadmin.sh
 # Run the script to update the configuration
