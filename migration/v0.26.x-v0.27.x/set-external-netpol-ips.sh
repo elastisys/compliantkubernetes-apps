c0974bce1d4c896ab397723f2cdad637474407d5
davidumea <dava.andersson@hotmail.com>
2022-11-24 09:30:01 +0100

    apps: made allow-falco netpol configurable

--- migration/v0.26.x-v0.27.x/set-external-netpol-ips.sh
+++ migration/v0.26.x-v0.27.x/set-external-netpol-ips.sh
@@ -4,6 +4,7 @@ set -e
 
 yq4 -i '.networkPolicies.global.trivy.ips[0] = "0.0.0.0/0"' "${CK8S_CONFIG_PATH}"/common-config.yaml
 yq4 -i '.networkPolicies.certManager.letsencrypt.ips[0] = "0.0.0.0/0"' "${CK8S_CONFIG_PATH}"/common-config.yaml
+yq4 -i '.networkPolicies.falco.plugins.ips[0] = "0.0.0.0/0"' "${CK8S_CONFIG_PATH}"/common-config.yaml
 yq4 -i '.networkPolicies.alertmanager.alertReceivers.ips[0] = "0.0.0.0/0"' "${CK8S_CONFIG_PATH}"/common-config.yaml
 
 yq4 -i '.networkPolicies.harbor.jobservice.ips[0] = "0.0.0.0/0"' "${CK8S_CONFIG_PATH}"/sc-config.yaml
