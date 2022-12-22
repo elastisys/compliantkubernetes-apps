f668e48247f8007da9ecd9bebe2da533f0b9ef96
crssnd <cristian.sandu@elastisys.com>
2022-12-22 13:53:33 +0100

    migration: added a migration file template

---
+++ migration/create-migration-document.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+set -euo pipefail
+
+usage() {
+    echo "This script must have apps old and new major versions as arguments."
+    echo "Usage: $0 [old_version] [new_version]"
+    echo "Example: ./create-migration-document.sh v0.22 v0.23"
+}
+if [  $# -lt 2 ]; then
+    usage
+    exit 1
+fi
+
+here="$(dirname "$(readlink -f "$0")")"
+export old_version="${1}"
+export new_version="${2}"
+folder_name="${here}/${old_version}.x-${new_version}.x"
+
+if [ -d "${folder_name}" ]; then
+    echo "- ${folder_name} directory exists"
+else
+    mkdir "${folder_name}"
+    echo "- ${folder_name} directory created"
+fi
+
+if [ -f "${folder_name}/upgrade-apps.md" ]; then
+    echo -n "- ${folder_name}/upgrade-apps.md exists. Do you want to replace it? (y/N): "
+    read -r reply
+    if [[ ${reply} =~ ^[yY]$ ]]; then
+        envsubst < "${here}/template/upgrade-apps.md" > "${folder_name}/upgrade-apps.md"
+        echo "- ${folder_name}/upgrade-apps.md replaced"
+    fi
+else
+    envsubst < "${here}/template/upgrade-apps.md" > "${folder_name}/upgrade-apps.md"
+    echo "- ${folder_name}/upgrade-apps.md created"
+fi
