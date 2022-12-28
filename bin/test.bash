0f092199983df0275c9bce046f7650854605c2cc
ayoubensalem <ayoubensalem@gmail.com>
2022-12-28 12:46:04 +0100

    tests: Add troubleshooting script for certificates/ingresses

--- bin/test.bash
+++ bin/test.bash
@@ -11,6 +11,11 @@ source "${here}/common.bash"
 # shellcheck source=pipeline/test/services/service-cluster/testOpensearch.sh
 source "${pipeline_path}/test/services/service-cluster/testOpensearch.sh"
 
+# shellcheck source=pipeline/test/services/service-cluster/testCertManager.sh
+source "${pipeline_path}/test/services/service-cluster/testCertManager.sh"
+# shellcheck source=pipeline/test/services/workload-cluster/testCertManager.sh
+source "${pipeline_path}/test/services/workload-cluster/testCertManager.sh"
+
 test_apps_sc() {
     log_info "Testing service cluster"
 
@@ -25,14 +30,23 @@ test_apps_wc() {
         "${pipeline_path}/test/services/test-wc.sh" "${config[config_file_wc]}"
 }
 
-function sc_help(){
-    printf "%s\n" "[Usage]: test sc [target] [ARGUMENTS]";
-    printf "%s\n" "List of targets:";
-    printf "\t%-23s %s\n" "opensearch" "Open search checks";
-    printf "\t%-23s %s\n" "fluentd" "Fluentd checks";
-    printf "\t%-23s %s\n" "opensearch-dashboards" "Opensearch dashboards checks";
-    printf "%s\n" "[NOTE] If no target is specified, the tests will go through all of them.";
-    exit 0;
+function sc_help() {
+    printf "%s\n" "[Usage]: test sc [target] [ARGUMENTS]"
+    printf "%s\n" "List of targets:"
+    printf "\t%-23s %s\n" "opensearch" "Open search checks"
+    printf "\t%-23s %s\n" "fluentd" "Fluentd checks"
+    printf "\t%-23s %s\n" "opensearch-dashboards" "Opensearch dashboards checks"
+    printf "\t%-23s %s\n" "cert-manager" "Cert Manager checks"
+    printf "%s\n" "[NOTE] If no target is specified, the tests will go through all of them."
+    exit 0
+}
+
+function wc_help() {
+    printf "%s\n" "[Usage]: test wc [target] [ARGUMENTS]"
+    printf "%s\n" "List of targets:"
+    printf "\t%-23s %s\n" "cert-manager" "Cert Manager checks"
+    printf "%s\n" "[NOTE] If no target is specified, the tests will go through all of them."
+    exit 0
 }
 
 function sc() {
@@ -40,48 +54,72 @@ function sc() {
         test_apps_sc
     else
         case ${1} in
-            opensearch )
-                sc_opensearch_checks "${@:2}";
+        opensearch)
+            sc_opensearch_checks "${@:2}"
             ;;
-            --help )
-              sc_help
+        cert-manager)
+            sc_cert_manager_checks "${@:2}"
             ;;
-            * )
-                echo "unknown command: $1";
-                sc_help 1;
-                exit 1;
+        --help)
+            sc_help
+            ;;
+        *)
+            echo "unknown command: $1"
+            sc_help 1
+            exit 1
             ;;
         esac
     fi
-    exit 0;
+    exit 0
 }
 
-function main_help(){
-    echo list of commands:
-    printf "%-23s %s\n" "help" "show help menu and commands";
-    printf "%-23s %s\n" "sc" "Run sc checks";
-    printf "%-23s %s\n" "wc" "Run wc checks";
-
-    exit "${1:-0}";
-}
-
-function main(){
+function wc() {
     if [[ ${#} == 0 ]]; then
-        main_help 0;
+        test_apps_wc
+    else
+        case ${1} in
+        cert-manager)
+            wc_cert_manager_checks "${@:2}"
+            ;;
+        --help)
+            wc_help
+            ;;
+        *)
+            echo "unknown command: $1"
+            wc_help 1
+            exit 1
+            ;;
+        esac
+    fi
+    exit 0
+}
+
+function main_help() {
+    echo list of commands:
+    printf "%-23s %s\n" "help" "show help menu and commands"
+    printf "%-23s %s\n" "sc" "Run sc checks"
+    printf "%-23s %s\n" "wc" "Run wc checks"
+
+    exit "${1:-0}"
+}
+
+function main() {
+    if [[ ${#} == 0 ]]; then
+        main_help 0
     fi
 
     case ${1} in
-        sc )
-            config_load "$1";
-            with_kubeconfig "${config[kube_config_sc]}" \
-              "$1" "${@:2}";
+    sc)
+        config_load "$1"
+        with_kubeconfig "${config[kube_config_sc]}" \
+            "$1" "${@:2}"
         ;;
-        wc )
-            config_load "$1";
-            with_kubeconfig "${config[kube_config_wc]}" \
-              "$1" "${@:2}";
+    wc)
+        config_load "$1"
+        with_kubeconfig "${config[kube_config_wc]}" \
+            "$1" "${@:2}"
         ;;
     esac
 }
 
-main "$@";
+main "$@"
