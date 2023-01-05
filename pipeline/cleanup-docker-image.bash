94bc66c4b0fc94ac979a42a79d321c68d084cd44
Fredrik Liv <fredrik.liv@elastisys.com>
2023-01-05 11:17:05 +0100

    pipeline: Updated to use github package

--- pipeline/cleanup-docker-image.bash
+++ pipeline/cleanup-docker-image.bash
@@ -1,24 +1,46 @@
 # Remove Docker image GITHUB_SHA tag created by pipeline from Docker Hub
-set -e
+set -eu -o pipefail
 
 if [ -z ${GITHUB_SHA+x} ]; then
     echo "GITHUB_SHA not set, skipping Docker tag deletion." >&2
 else
-    : "${DOCKERHUB_USERNAME:?Missing DOCKERHUB_USERNAME}"
-    : "${DOCKERHUB_PASSWORD:?Missing DOCKERHUB_PASSWORD}"
-    : "${DOCKERHUB_REPOSITORY:?Missing DOCKERHUB_REPOSITORY}"
+    : "${GITHUB_ACTOR:?Missing GITHUB_ACTOR}"
+    : "${GITHUB_TOKEN:?Missing GITHUB_TOKEN}"
 
-    echo "Deleting Docker Hub tag: ${GITHUB_SHA}" >&2
+    # At the moment you need to know the version id to delete a container version.
+    # To fetch it you need to list all versions and fetch the ID of the one with the correct tag.
+    # One entry in the list of versions looks something like this:
+    #   {
+    #     "id": 123456,
+    #     ...
+    #     "metadata": {
+    #       "container": {
+    #         "tags": [
+    #           "${GITHUB_SHA}"
+    #         ]
+    #       }
+    #     }
+    #   }
+    # So to fetch the id we need to match to any entry that has the commit hash and fetch that id
+    # The jq filter here is not foolproof and can return multiple versions if there's more than one version ID with the same tag.
+    VERSION_ID=$(curl -s \
+      -H "Accept: application/vnd.github+json" \
+      -H "Authorization: Bearer ${GITHUB_TOKEN}" \
+      -H "X-GitHub-Api-Version: 2022-11-28" \
+      https://api.github.com/orgs/elastisys/packages/container/compliantkubernetes-apps-pipeline/versions | \
+      jq '.[] | select(.metadata.container.tags | any(. == "'"${GITHUB_SHA}"'")).id')
 
-    TOKEN=$(curl -s -X POST \
-            -H "Content-Type: application/json" \
-            -d '{"username": "'"${DOCKERHUB_USERNAME}"'",
-                 "password": "'"${DOCKERHUB_PASSWORD}"'"}' \
-                "https://hub.docker.com/v2/users/login/" | \
-            jq -r .token)
+    echo "Deleting Github package tag: ${GITHUB_SHA} (Version ID: ${VERSION_ID})" >&2
 
-    curl -i -f -X DELETE \
-        -H "Accept: application/json" \
-        -H "Authorization: JWT ${TOKEN}" \
-        "https://hub.docker.com/v2/repositories/${DOCKERHUB_REPOSITORY}/tags/${GITHUB_SHA}/"
+    if [[ ! "${VERSION_ID}" =~ ^[0-9]+$ ]]; then
+      echo "Version ID not correctly formatted" >&2
+      exit 1
+    fi
+
+    curl -s \
+      -X DELETE \
+      -H "Accept: application/vnd.github+json" \
+      -H "Authorization: Bearer ${GITHUB_TOKEN}"\
+      -H "X-GitHub-Api-Version: 2022-11-28" \
+      "https://api.github.com/orgs/elastisys/packages/container/compliantkubernetes-apps-pipeline/versions/${VERSION_ID}"
 fi
