c4ce2e2966c27e76e777889c94e597b63fb83721
Ayoub Ed-dafali <ayoub.eddafali@gmail.com>
2023-01-12 10:48:46 +0100

    tests: Add troubleshooting script for certificates/ingress (#1356)

--- bin/test.bash
+++ bin/test.bash
@@ -11,6 +11,15 @@ source "${here}/common.bash"
 # shellcheck source=pipeline/test/services/service-cluster/testOpensearch.sh
 source "${pipeline_path}/test/services/service-cluster/testOpensearch.sh"
 
+# shellcheck source=pipeline/test/services/service-cluster/testCertManager.sh
+source "${pipeline_path}/test/services/service-cluster/testCertManager.sh"
+# shellcheck source=pipeline/test/services/workload-cluster/testCertManager.sh
+source "${pipeline_path}/test/services/workload-cluster/testCertManager.sh"
+# shellcheck source=pipeline/test/services/service-cluster/testIngress.sh
+source "${pipeline_path}/test/services/service-cluster/testIngress.sh"
+# shellcheck source=pipeline/test/services/workload-cluster/testIngress.sh
+source "${pipeline_path}/test/services/workload-cluster/testIngress.sh"
+
 test_apps_sc() {
     log_info "Testing service cluster"
 
@@ -29,7 +38,18 @@ function sc_help() {
     printf "%s\n" "[Usage]: test sc [target] [ARGUMENTS]"
     printf "%s\n" "List of targets:"
     printf "\t%-23s %s\n" "opensearch" "Open search checks"
-    printf "%s\n" "[NOTE] If no target is specified, the tests will go through all of them."
+    printf "\t%-23s %s\n" "cert-manager" "Cert Manager checks"
+    printf "\t%-23s %s\n" "ingress" "Ingress checks"
+    printf "%s\n" "[NOTE] If no target is specified, the default sc apps tests will be executed."
+    exit 0
+}
+
+function wc_help() {
+    printf "%s\n" "[Usage]: test wc [target] [ARGUMENTS]"
+    printf "%s\n" "List of targets:"
+    printf "\t%-23s %s\n" "cert-manager" "Cert Manager checks"
+    printf "\t%-23s %s\n" "ingress" "Ingress checks"
+    printf "%s\n" "[NOTE] If no target is specified, the default wc apps tests will be executed."
     exit 0
 }
 
@@ -41,12 +61,42 @@ function sc() {
         opensearch)
             sc_opensearch_checks "${@:2}"
             ;;
+        cert-manager)
+            sc_cert_manager_checks "${@:2}"
+            ;;
+        ingress)
+            sc_ingress_checks "${@:2}"
+            ;;
         --help)
             sc_help
             ;;
         *)
             echo "unknown command: $1"
             sc_help 1
+            exit 1
+            ;;
+        esac
+    fi
+    exit 0
+}
+
+function wc() {
+    if [[ ${#} == 0 ]]; then
+        test_apps_wc
+    else
+        case ${1} in
+        cert-manager)
+            wc_cert_manager_checks "${@:2}"
+            ;;
+        ingress)
+            wc_ingress_checks "${@:2}"
+            ;;
+        --help)
+            wc_help
+            ;;
+        *)
+            echo "unknown command: $1"
+            wc_help 1
             exit 1
             ;;
         esac
