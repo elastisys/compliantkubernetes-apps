30a07f1a97ab1fbafe2114d65a496f8d963ce18b
crssnd <cristian.sandu@elastisys.com>
2023-04-12 10:44:18 +0200

    apps: fix kube-prometheus-stack migration script

--- migration/v0.30/apply/20-remove-kube-prometheus-components.sh
+++ migration/v0.30/apply/20-remove-kube-prometheus-components.sh
@@ -9,11 +9,27 @@ run() {
   case "${1:-}" in
   execute)
     for CLUSTER in sc wc; do
-      log_info "  - removing kube-state-metrics from $CLUSTER"
-      kubectl_do $CLUSTER delete deployments.apps -l app.kubernetes.io/instance=kube-prometheus-stack,app.kubernetes.io/name=kube-state-metrics --cascade=orphan -n monitoring
-      log_info "  - removing prometheus-node-exporter from $CLUSTER"
-      kubectl_do $CLUSTER delete daemonset -l app=prometheus-node-exporter -n monitoring
+      old_state_metrics=$(kubectl get deployments.apps -n monitoring -l app.kubernetes.io/instance=kube-prometheus-stack,app.kubernetes.io/name=kube-state-metrics,app.kubernetes.io/version!=2.8.0 --output=jsonpath={.items..metadata.name})
+      old_node_exporter=$(kubectl get daemonset -n monitoring -l app=prometheus-node-exporter --output=jsonpath={.items..metadata.name})
+
+      if [[ -n "${old_state_metrics}" ]]; then
+        log_info "- removing kube-state-metrics from $CLUSTER"
+        kubectl_do $CLUSTER delete deployments.apps -l app.kubernetes.io/instance=kube-prometheus-stack,app.kubernetes.io/name=kube-state-metrics,app.kubernetes.io/version!=2.8.0 --cascade=orphan -n monitoring
+      fi
+
+      if [[ -n "${old_node_exporter}" ]]; then
+        log_info "- removing prometheus-node-exporter from $CLUSTER"
+        kubectl_do $CLUSTER delete daemonset -l app=prometheus-node-exporter -n monitoring
+      fi
     done
+
+    # --- sc ---
+    helmfile_upgrade sc app=kube-prometheus-stack
+    helmfile_upgrade sc app=thanos
+
+    # --- wc ---
+    helmfile_upgrade wc app=kube-prometheus-stack
+    helmfile_upgrade wc app=user-alertmanager
     ;;
   rollback)
     log_warn "rollback not implemented"
