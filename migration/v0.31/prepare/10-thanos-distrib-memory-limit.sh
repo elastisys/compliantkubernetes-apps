318bea3b4b76384ca810628e9d16ca6a6419696f
crssnd <cristian.sandu@elastisys.com>
2023-07-25 15:48:55 +0200

    fix: comments

--- migration/v0.31/prepare/10-thanos-distrib-memory-limit.sh
+++ migration/v0.31/prepare/10-thanos-distrib-memory-limit.sh
@@ -8,11 +8,11 @@ source "${ROOT}/scripts/migration/lib.sh"
 
 if ! yq_null sc .thanos.receiveDistributor.resources.limits.memory; then
   log_info "- check if thanos distributor memory limit is less than 700Mi"
-  size=$(yq4 '.thanos.receiveDistributor.resources.limits.memory | sub("Mi","")' "${CK8S_CONFIG_PATH}/sc-config.yaml")
-  if ((size < 700)); then
+  size=$(yq4 '.thanos.receiveDistributor.resources.limits.memory | select(. == "*Mi") | sub("Mi","")' "${CK8S_CONFIG_PATH}/sc-config.yaml")
+  if [ -n "$size" ] && ((size < 700)); then
     log_info "- increase the thanos distributor memory limit to 700Mi"
     yq4 -i '.thanos.receiveDistributor.resources.limits.memory = "700Mi"' "${CK8S_CONFIG_PATH}/sc-config.yaml"
   else
-    log_info "- thanos distributor memory limit is $size, will not update it"
+    log_info "- thanos distributor memory limit is greater or equal with 700Mi, will not update it"
   fi
 fi
