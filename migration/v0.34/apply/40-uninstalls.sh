fdab08ff4986f0d1ad7711bf1c39d3d2569ea1a2
Pavan-Gunda <pavan.gunda@elastisys.com>
2023-11-28 10:33:28 +0100

    Upgraded trivy operator from v0.13.0 to v0.17.0

---
+++ migration/v0.34/apply/40-uninstalls.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+ROOT="$(readlink -f "$(dirname "${0}")/../../../")"
+
+# shellcheck source=scripts/migration/lib.sh
+source "${ROOT}/scripts/migration/lib.sh"
+
+run() {
+  case "${1:-}" in
+  execute)
+    helm_uninstall sc monitoring trivy-operator
+    helm_uninstall wc monitoring trivy-operator
+    ;;
+  rollback)
+    log_warn "rollback not implemented"
+    ;;
+  *)
+    log_fatal "usage: \"${0}\" <execute|rollback>"
+    ;;
+  esac
+}
+
+run "${@}"
