29cf4d23720a3806c38700915672257462f6b76f
Pavan-Gunda <pavan.gunda@elastisys.com>
2023-11-28 11:58:11 +0100

    fix: modified scripts to be applied on individual clusters

--- migration/v0.34/apply/40-uninstalls.sh
+++ migration/v0.34/apply/40-uninstalls.sh
@@ -8,8 +8,12 @@ source "${ROOT}/scripts/migration/lib.sh"
 run() {
   case "${1:-}" in
   execute)
-    helm_uninstall sc monitoring trivy-operator
-    helm_uninstall wc monitoring trivy-operator
+    if [[ "${CK8S_CLUSTER}" =~ ^(sc|both)$ ]]; then
+      helm_uninstall sc monitoring trivy-operator
+    fi
+    if [[ "${CK8S_CLUSTER}" =~ ^(wc|both)$ ]]; then
+      helm_uninstall wc monitoring trivy-operator
+    fi
     ;;
   rollback)
     log_warn "rollback not implemented"
