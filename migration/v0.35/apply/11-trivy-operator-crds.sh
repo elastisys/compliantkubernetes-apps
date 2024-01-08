818baad8f94004de9682762e300cd90fcfa98dc4
crssnd <cristian.sandu@elastisys.com>
2024-01-08 10:17:41 +0100

    fix: upgrade trivy migration script

--- migration/v0.35/apply/11-trivy-operator-crds.sh
+++ migration/v0.35/apply/11-trivy-operator-crds.sh
@@ -9,12 +9,20 @@ run() {
   case "${1:-}" in
   execute)
     if [[ "${CK8S_CLUSTER}" =~ ^(sc|both)$ ]]; then
-      log_info "  - applying the trivy-operator CRDs on sc"
-      kubectl_do sc apply --server-side --force-conflicts -f "${ROOT}"/helmfile/upstream/aquasecurity/trivy-operator/crds
+      if [[ "$(yq_dig sc .trivy.enabled)" == "true" && "$(helm_chart_version sc monitoring trivy-operator)" != "0.19.1" ]]; then
+        log_info "  - applying the trivy-operator CRDs on sc"
+        kubectl_do sc apply --server-side --force-conflicts -f "${ROOT}"/helmfile/upstream/aquasecurity/trivy-operator/crds
+        log_info "  - upgrade trivy-operator on sc"
+        kubectl_do sc app=trivy-operator
+      fi
     fi
     if [[ "${CK8S_CLUSTER}" =~ ^(wc|both)$ ]]; then
-      log_info "  - applying the trivy-operator CRDs on wc"
-      kubectl_do wc apply --server-side --force-conflicts -f "${ROOT}"/helmfile/upstream/aquasecurity/trivy-operator/crds
+      if [[ "$(yq_dig wc .trivy.enabled)" == "true" && "$(helm_chart_version wc monitoring trivy-operator)" != "0.19.1" ]]; then
+        log_info "  - applying the trivy-operator CRDs on wc"
+        kubectl_do wc apply --server-side --force-conflicts -f "${ROOT}"/helmfile/upstream/aquasecurity/trivy-operator/crds
+        log_info "  - upgrade trivy-operator on wc"
+        kubectl_do wc app=trivy-operator
+      fi
     fi
     ;;
   rollback)
