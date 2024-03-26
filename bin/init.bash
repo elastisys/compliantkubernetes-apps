f0e82a0ccec533406cae7c030ef40aec571faa32
vomba <hani.harzallah@elastisys.com>
2024-03-26 14:46:27 +0100

    add set_cluster_api

--- bin/init.bash
+++ bin/init.bash
@@ -421,6 +421,30 @@ set_harbor_config() {
     replace_set_me "${file}" ".harbor.persistence.disableRedirect" "${disable_redirect}"
 }
 
+
+# Usage: set_cluster_api <config-file>
+set_cluster_api() {
+    file="${1}"
+    if [[ ! -f "${file}" ]]; then
+        log_error "ERROR: invalid file - ${file}"
+        exit 1
+    fi
+    case ${CLUSTER_PROVISIONER} in
+        cluster-api)
+            clusterapi=true
+        ;;
+        kubespray)
+            clusterapi=false
+        ;;
+        *)
+            clusterapi=false
+    esac
+
+    replace_set_me "${file}" ".clusterApi.enabled" "${clusterapi}"
+    replace_set_me "${file}" ".clusterApi.monitoring.enabled" "${clusterapi}"
+    replace_set_me "${file}" ".kubeStateMetrics.clusterAPIMetrics.enabled" "${clusterapi}"
+}
+
 # Usage: set_cluster_dns <config-file>
 set_cluster_dns() {
     file="${1}"
@@ -430,17 +454,14 @@ set_cluster_dns() {
     fi
     case ${CLUSTER_PROVISIONER} in
         cluster-api)
-            clusterapi=true
             clusterdns="10.233.0.10"
             clusterdnscidr="10.233.0.10/32"
         ;;
         kubespray)
-            clusterapi=false
             clusterdns="10.233.0.3"
             clusterdnscidr="10.233.0.3/32"
         ;;
         *)
-            clusterapi=false
             clusterdns="set-me"
             clusterdnscidr="set-me"
         ;;
@@ -448,9 +469,6 @@ set_cluster_dns() {
 
     replace_set_me "${file}" ".global.clusterDns" "\"${clusterdns}\""
     replace_set_me "${file}" '.networkPolicies.coredns.serviceIp.ips' "[\"${clusterdnscidr}\"]"
-    replace_set_me "${file}" ".clusterApi.enabled" "${clusterapi}"
-    replace_set_me "${file}" ".clusterApi.monitoring.enabled" "${clusterapi}"
-    replace_set_me "${file}" ".kubeStateMetrics.clusterAPIMetrics.enabled" "${clusterapi}"
 }
 
 update_monitoring() {
@@ -730,6 +748,7 @@ set_storage_class       "${config[default_common]}"
 set_object_storage      "${config[default_common]}"
 set_nginx_config        "${config[default_common]}"
 set_lbsvc_safeguard     "${config[default_common]}"
+set_cluster_api         "${config[default_common]}"
 set_cluster_dns         "${config[default_common]}"
 update_monitoring       "${config[default_common]}"
 update_psp_netpol       "${config[default_common]}"
