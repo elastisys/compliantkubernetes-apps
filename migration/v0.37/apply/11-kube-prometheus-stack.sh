9741f2aa04de3b75c64ee5ec4bc482e6d7fce725
crssnd <cristian.sandu@elastisys.com>
2024-04-02 15:01:13 +0200

    scripts: changes to the v0.37 migration scripts

--- migration/v0.37/apply/11-kube-prometheus-stack.sh
+++ migration/v0.37/apply/11-kube-prometheus-stack.sh
@@ -10,14 +10,14 @@ run() {
 
     if [[ "${CK8S_CLUSTER}" =~ ^(sc|both)$ ]]; then
       log_info "  - Replace kube-prometheus-stack CRDs on sc"
-      kubectl_do sc apply --server-side -f "${ROOT}"/helmfile.d/upstream/prometheus-community/kube-prometheus-stack/charts/crds/crds
+      kubectl_do sc apply --server-side --force-conflicts -f "${ROOT}"/helmfile.d/upstream/prometheus-community/kube-prometheus-stack/charts/crds/crds
 
       log_info "  - Upgrade kube-prometheus-stack on sc"
       helmfile_upgrade sc app=prometheus
     fi
     if [[ "${CK8S_CLUSTER}" =~ ^(wc|both)$ ]]; then
       log_info "  - Replace kube-prometheus-stack CRDs on wc"
-      kubectl_do wc apply --server-side -f "${ROOT}"/helmfile.d/upstream/prometheus-community/kube-prometheus-stack/charts/crds/crds
+      kubectl_do wc apply --server-side --force-conflicts -f "${ROOT}"/helmfile.d/upstream/prometheus-community/kube-prometheus-stack/charts/crds/crds
 
       log_info "  - Upgrade kube-prometheus-stack on wc"
       helmfile_upgrade wc app=prometheus
