6d4d2df24679f3fd522a22cafc1347d4d40c2984
Elias Häreskog <elias.hareskog@elastisys.com>
2024-05-02 16:25:09 +0200

    apps: improve migration script for trivy-operator

--- migration/v0.38/apply/10-trivy-operator-upgrade.sh
+++ migration/v0.38/apply/10-trivy-operator-upgrade.sh
@@ -8,13 +8,23 @@ source "${ROOT}/scripts/migration/lib.sh"
 run() {
   case "${1:-}" in
   execute)
-
+    if [[ "${CK8S_CLUSTER}" =~ ^(sc|both)$ ]]; then
+      if [[ "$(yq_dig sc .trivy.enabled)" == "true" && "$(helm_chart_version sc monitoring trivy-operator)" != "0.22.1" ]]; then
+        log_info "Upgrading trivy-operator in sc"
+        kubectl_do sc delete crd sbomreports.aquasecurity.github.io
+        kubectl_do sc delete crd clustersbomreports.aquasecurity.github.io
+        kubectl_do sc apply -f "${ROOT}"/helmfile.d/upstream/aquasecurity/trivy-operator/crds
+        helmfile_do sc -l name=trivy-operator apply
+      fi
+    fi
     if [[ "${CK8S_CLUSTER}" =~ ^(wc|both)$ ]]; then
-      log_info "Upgrading trivy-operator"
-      kubectl_do wc delete crd sbomreports.aquasecurity.github.io
-      kubectl_do wc delete crd clustersbomreports.aquasecurity.github.io
-      helmfile_do wc -l name=trivy-operator apply
-      kubectl_do wc apply -f "${ROOT}"/helmfile.d/upstream/aquasecurity/trivy-operator/crds
+      if [[ "$(yq_dig wc .trivy.enabled)" == "true" && "$(helm_chart_version wc monitoring trivy-operator)" != "0.22.1" ]]; then
+        log_info "Upgrading trivy-operator in wc"
+        kubectl_do wc delete crd sbomreports.aquasecurity.github.io
+        kubectl_do wc delete crd clustersbomreports.aquasecurity.github.io
+        kubectl_do wc apply -f "${ROOT}"/helmfile.d/upstream/aquasecurity/trivy-operator/crds
+        helmfile_do wc -l name=trivy-operator apply
+      fi
     fi
     ;;
   rollback)
