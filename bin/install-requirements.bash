e82038bbd5167d8e93a6c08e5e829d065dadb47c
anders-elastisys <anders.backman@elastisys.com>
2024-05-08 08:51:30 +0200

    fix: noerror AAAA nodelocaldns + opensearch app version bump

--- bin/install-requirements.bash
+++ bin/install-requirements.bash
@@ -6,4 +6,4 @@ export ANSIBLE_STDOUT_CALLBACK=yaml
 
 echo "This script will install all requirements"
 echo "Enter your password to start the install"
-ansible-playbook -e 'ansible_python_interpreter=/usr/bin/python3' --ask-become-pass --connection local --inventory 127.0.0.1, roles/get-requirements.yaml
+ansible-playbook -e 'ansible_python_interpreter=/usr/bin/python3' -b --connection local --inventory 127.0.0.1, roles/get-requirements.yaml
