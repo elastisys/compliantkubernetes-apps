9a6f5fa4aefa632c7be7b2fd4e38c9f22239566c
Simon Kollberg <simonkollberg@gmail.com>
2024-05-17 15:32:24 +0200

    bin: Support installing requirements without root in $HOME/bin

--- bin/install-requirements.bash
+++ bin/install-requirements.bash
@@ -6,4 +6,9 @@ export ANSIBLE_STDOUT_CALLBACK=yaml
 
 echo "This script will install all requirements"
 echo "Enter your password to start the install"
-ansible-playbook -e 'ansible_python_interpreter=/usr/bin/python3' --ask-become-pass --connection local --inventory 127.0.0.1, roles/get-requirements.yaml
+
+if [ "${#}" -ge 2 ] && [ "${2}" = "--user" ]; then
+  ansible-playbook -e 'ansible_python_interpreter=/usr/bin/python3' --ask-become-pass --extra-vars install_path="${HOME}/bin" roles/get-requirements.yaml
+else
+  ansible-playbook -e 'ansible_python_interpreter=/usr/bin/python3' --ask-become-pass --become --become-user root roles/get-requirements.yaml
+fi
