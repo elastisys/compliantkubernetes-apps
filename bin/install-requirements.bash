703190a596f0573264c554404a26ae1eea6e4cc3
Simon Kollberg <simonkollberg@gmail.com>
2024-05-17 16:25:37 +0200

    bin: Add requirements install path env var CK8S_INSTALL_PATH

--- bin/install-requirements.bash
+++ bin/install-requirements.bash
@@ -8,7 +8,8 @@ echo "This script will install all requirements"
 echo "Enter your password to start the install"
 
 if [ "${#}" -ge 2 ] && [ "${2}" = "--user" ]; then
-  ansible-playbook -e 'ansible_python_interpreter=/usr/bin/python3' --ask-become-pass --extra-vars install_path="${HOME}/bin" roles/get-requirements.yaml
+  export CK8S_INSTALL_PATH="${CK8S_INSTALL_PATH:-"${HOME}/bin"}"
+  ansible-playbook -e 'ansible_python_interpreter=/usr/bin/python3' --ask-become-pass roles/get-requirements.yaml
 else
   ansible-playbook -e 'ansible_python_interpreter=/usr/bin/python3' --ask-become-pass --become --become-user root roles/get-requirements.yaml
 fi
