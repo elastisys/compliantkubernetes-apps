c9a211aa2464811afc5421b07a9e33932a217640
Axel Jarmar <axel.jarmar@elastisys.com>
2024-06-14 15:45:06 +0200

    remove unused function

--- bin/init.bash
+++ bin/init.bash
@@ -86,23 +86,6 @@ generate_sops_config() {
     sops_config_write_fingerprints "${fingerprint}"
 }
 
-# Only writes value if it is set to "set-me", or a conditional "set-me" and the condition is true
-# Usage: replace_set_me <file> <field> <value>
-replace_set_me(){
-    if [[ $# -ne 3 ]]; then
-        log_error "ERROR: number of args in replace_set_me must be 3. #=[$#]"
-        exit 1
-    fi
-    if [[ $(yq4 "${2}" "${1}") == "set-me" ]]; then
-        yq4 --inplace "${2} = ${3}" "${1}"
-    elif [[ $(yq4 "${2}" "${1}") =~ ^set\-me\-if\-.*$ ]]; then
-        required_condition=$(yq4 "${2}" "${1}" | sed -rn 's/^set-me-if-(.*)/\1/p')
-        if [[ $(yq4 "${required_condition}" "${1}") == "true" ]]; then
-            yq4 --inplace "${2} = ${3}" "${1}"
-        fi
-    fi
-}
-
 # Usage: generate_default_config <default_config>
 generate_default_config() {
     if [[ $# -ne 1 ]]; then
