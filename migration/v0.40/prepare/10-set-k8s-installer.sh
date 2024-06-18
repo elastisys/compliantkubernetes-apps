5d02ac87bddd76ec69abb1ac27d9e269fa3ebb86
anders-elastisys <anders.backman@elastisys.com>
2024-06-18 14:10:24 +0200

    bin: add v0.40 migration scripts and set k8s-installer step

---
+++ migration/v0.40/prepare/10-set-k8s-installer.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+HERE="$(dirname "$(readlink -f "${0}")")"
+ROOT="$(readlink -f "${HERE}/../../../")"
+
+# shellcheck source=scripts/migration/lib.sh
+source "${ROOT}/scripts/migration/lib.sh"
+
+cluster="${CK8S_CLUSTER}"
+if [[ "${CK8S_CLUSTER}" == both ]]; then
+  cluster="wc"
+fi
+
+# check if kubespray directory exists
+if [[ -d "${CK8S_CONFIG_PATH}/${cluster}-config" ]]; then
+  yq_add common .global.k8sInstaller "\"kubespray\""
+else
+  yq_add common .global.k8sInstaller "\"capi\""
+fi
