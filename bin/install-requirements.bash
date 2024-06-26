83a046221ea10ac392392e6ad56c2f687406c2ce
Albin Björk <91016401+AlbinB97@users.noreply.github.com>
2024-06-26 15:11:24 +0200

    bin: allow password-less sudo for install-requirements script

--- bin/install-requirements.bash
+++ bin/install-requirements.bash
@@ -7,9 +7,13 @@ export ANSIBLE_STDOUT_CALLBACK=yaml
 echo "This script will install all requirements"
 echo "Enter your password to start the install"
 
-if [ "${#}" -ge 2 ] && [ "${2}" = "--user" ]; then
+if [ "${#}" -ge 1 ] && [ "${1}" = "--user" ]; then
   export CK8S_INSTALL_PATH="${CK8S_INSTALL_PATH:-"${HOME}/bin"}"
   ansible-playbook -e 'ansible_python_interpreter=/usr/bin/python3' --ask-become-pass roles/get-requirements.yaml
 else
-  ansible-playbook -e 'ansible_python_interpreter=/usr/bin/python3' --ask-become-pass --become --become-user root roles/get-requirements.yaml
+  if [ "${#}" -ge 1 ] && [ "${1}" = "--no-pass" ]; then
+    ansible-playbook -e 'ansible_python_interpreter=/usr/bin/python3' --become --become-user root roles/get-requirements.yaml
+  else
+    ansible-playbook -e 'ansible_python_interpreter=/usr/bin/python3' --ask-become-pass --become --become-user root roles/get-requirements.yaml
+  fi
 fi
