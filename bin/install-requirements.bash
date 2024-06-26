618d1a0f2e4f5c8df121b5291dd495fca05726a7
AlbinB97 <devalbinbjork@gmail.com>
2024-06-26 11:53:46 +0200

    bin: allow passwordless sudo of install requirements

--- bin/install-requirements.bash
+++ bin/install-requirements.bash
@@ -11,5 +11,9 @@ if [ "${#}" -ge 2 ] && [ "${2}" = "--user" ]; then
   export CK8S_INSTALL_PATH="${CK8S_INSTALL_PATH:-"${HOME}/bin"}"
   ansible-playbook -e 'ansible_python_interpreter=/usr/bin/python3' --ask-become-pass roles/get-requirements.yaml
 else
-  ansible-playbook -e 'ansible_python_interpreter=/usr/bin/python3' --ask-become-pass --become --become-user root roles/get-requirements.yaml
+  if [ "${#}" -ge 2 ] && [ "${2}" = "--become" ]; then
+    ansible-playbook -e 'ansible_python_interpreter=/usr/bin/python3' --become --become-user root roles/get-requirements.yaml
+  else
+    ansible-playbook -e 'ansible_python_interpreter=/usr/bin/python3' --ask-become-pass --become --become-user root roles/get-requirements.yaml
+  fi
 fi
