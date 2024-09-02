9d5a19c52067a52abd2d3d6abc03b356626ae23a
Simon Kollberg <simonkollberg@gmail.com>
2024-09-02 17:06:14 +0200

    tests: Fix Harbor ctr.insecure execution

--- tests/common/bats/ctr.bash
+++ tests/common/bats/ctr.bash
@@ -13,7 +13,6 @@ ctr() {
 ctr.insecure() {
   if docker version >/dev/null 2>&1 && [[ ! "$(docker version)" =~ Podman ]]; then
     docker --tlsverify=false "${@}"
-    echo "${ctr_insecure+"--tlsverify=false"}"
   else
     podman "${1}" --tls-verify=false "${@:2}"
   fi
