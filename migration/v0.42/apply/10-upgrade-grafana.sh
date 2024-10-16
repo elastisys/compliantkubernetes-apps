e3f7f4585ac5e69102c0c673551a0f814faaa767
Elias Lindkvist <elias.lindkvist@elastisys.com>
2024-10-16 09:44:23 +0200

    all: fix grafana datasources and tests for multi wc

--- migration/v0.42/apply/10-upgrade-grafana.sh
+++ migration/v0.42/apply/10-upgrade-grafana.sh
@@ -18,12 +18,36 @@ run() {
         log_info "Deleting Grafana release secrets."
         kubectl_do sc delete secrets -n monitoring -l name=ops-grafana,owner=helm
         kubectl_do sc delete secrets -n monitoring -l name=user-grafana,owner=helm
-
-        helmfile_apply sc app=grafana
       else
         log_info "Grafana releases are up to date, skipping"
       fi
+      helmfile_apply sc app=grafana
+      kubectl_do sc wait pod -n monitoring --for=condition=ready -l app.kubernetes.io/instance=user-grafana --timeout=120s
 
+      if [[ $(yq4 '.global.clustersMonitoring | length' "${CK8S_CONFIG_PATH}/sc-config.yaml") -gt 1 ]]; then
+        clusters_monitoring=$(yq4 '[.global.clustersMonitoring[] | {"name": .}]' "${CK8S_CONFIG_PATH}/sc-config.yaml" -ojson)
+        export clusters_monitoring
+
+        user_grafana_cm=$(kubectl_do sc get cm user-grafana -n monitoring -o=jsonpath='{.data.datasources\.yaml}' \
+          | yq4 '.deleteDatasources = env(clusters_monitoring)' -o json)
+
+        PATCH="[
+          {
+            'op': 'replace',
+            'path': '/data/datasources.yaml',
+            'value': '${user_grafana_cm}'
+          }
+        ]"
+        kubectl_do sc patch cm user-grafana -n monitoring --type json --patch "${PATCH}"
+        kubectl_do sc rollout restart deployment user-grafana -n monitoring
+        kubectl_do sc rollout status deployment user-grafana -n monitoring
+
+        kubectl_do sc delete configmap user-grafana -n monitoring
+        helmfile_do sc -l name=user-grafana sync
+
+        kubectl_do sc rollout restart deployment user-grafana -n monitoring
+        kubectl_do sc rollout status deployment user-grafana -n monitoring
+      fi
     fi
     ;;
   rollback)
