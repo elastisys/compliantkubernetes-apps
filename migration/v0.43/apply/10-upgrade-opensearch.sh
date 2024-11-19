a8c95aa05b0b18a108e80211419f238bb863bbe1
Simon Lundkvist <simon.lundkvist@elastisys.com>
2024-11-19 13:01:19 +0100

    apps sc: upgrade opensearch to v2.17.1

---
+++ migration/v0.43/apply/10-upgrade-opensearch.sh
@@ -0,0 +1,38 @@
+#!/usr/bin/env bash
+
+ROOT="$(readlink -f "$(dirname "${0}")/../../../")"
+
+# shellcheck source=scripts/migration/lib.sh
+source "${ROOT}/scripts/migration/lib.sh"
+
+run() {
+  case "${1:-}" in
+  execute)
+    # Note: 00-template.sh will be skipped by the upgrade command
+    log_info "Upgrading prometheus-opensearch-exporter"
+
+    if [[ "${CK8S_CLUSTER}" =~ ^(sc|both)$ ]]; then
+      log_info "operation on service cluster"
+      log_info "- Removing opensearch-configurer"
+      helmfile_destroy sc name=opensearch-configurer
+      log_info "- Upgrading Opensearch"
+      helmfile_do sc -lapp=opensearch sync
+    fi
+    ;;
+  rollback)
+    log_warn "rollback not implemented"
+
+    # if [[ "${CK8S_CLUSTER}" =~ ^(sc|both)$ ]]; then
+    #   log_info "rollback operation on service cluster"
+    # fi
+    # if [[ "${CK8S_CLUSTER}" =~ ^(wc|both)$ ]]; then
+    #   log_info "rollback operation on workload cluster"
+    # fi
+    ;;
+  *)
+    log_fatal "usage: \"${0}\" <execute|rollback>"
+    ;;
+  esac
+}
+
+run "${@}"
