9f3142e135beb294b4a344a9fe345edbaf859248
Simon Kollberg <simonkollberg@gmail.com>
2025-02-19 16:17:48 +0100

    WIP

--- modules/hack/log-demo.bash
+++ modules/hack/log-demo.bash
@@ -6,12 +6,16 @@ here="$(dirname "$(readlink -f "$0")")"
 
 root="${here}/../.."
 
-# "${root}/bin/ck8s" ops helmfile sc diff -l name=ingress-nginx --include-transitive-needs --output simple
-# "${root}/bin/ck8s" ops helmfile sc apply -l name=ingress-nginx --include-transitive-needs
+"${root}/bin/ck8s" ops helmfile sc apply -l name=ingress-nginx --include-transitive-needs
 
-# "${root}/bin/ck8s" ops helmfile sc diff -l name=module-opensearch --include-transitive-needs --show-secrets
-# "${root}/bin/ck8s" ops helmfile sc diff -l name=module-opensearch --include-transitive-needs --output simple
+# TODO: update-ips
+
 "${root}/bin/ck8s" ops helmfile sc apply -l name=module-opensearch --include-transitive-needs
 
-# "${root}/bin/ck8s" ops helmfile wc diff -l name=module-fluentd-forwarder --include-transitive-needs --output simple
-# "${root}/bin/ck8s" ops helmfile wc apply -l name=module-fluentd-forwarder --include-transitive-needs
+"${root}/bin/ck8s" ops helmfile sc apply -l name=opensearch-securityadmin --include-transitive-needs
+
+"${root}/bin/ck8s" ops helmfile sc apply -l name=opensearch-configurer --include-transitive-needs
+
+"${root}/bin/ck8s" ops helmfile wc apply -l name=module-fluentd-forwarder --include-transitive-needs
+
+# TODO: patch secret, show propagation
