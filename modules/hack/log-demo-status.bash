9d71bafe5394c80f0b4ab076fb070ad15af1540c
Simon Kollberg <simonkollberg@gmail.com>
2025-02-27 11:55:14 +0100

    Improve demo visuals

--- modules/hack/log-demo-status.bash
+++ modules/hack/log-demo-status.bash
@@ -25,70 +25,75 @@ print_ds_env() {
 }
 
 search() {
-  # To remove some of the more spammy ones:
-  # "query": {
-  #   "bool": {
-  #     "must": [],
-  #     "filter": [
-  #       {
-  #         "match_all": {}
-  #       }
-  #     ],
-  #     "should": [],
-  #     "must_not": [
-  #       {
-  #         "match_phrase": {
-  #           "kubernetes.container_name": "etcd"
-  #         }
-  #       }, {
-  #         "match_phrase": {
-  #           "kubernetes.container_name": "calico-node"
-  #         }
-  #       }, {
-  #         "match_phrase": {
-  #           "kubernetes.container_name": "kube-apiserver"
-  #         }
-  #       }
-  #     ]
-  #   }
-  # }
+  date_from=$(date -d "15 minutes ago" -u +"%Y-%m-%dT%H:%M:%SZ")
+  date_now=$(date -u +"%Y-%m-%dT%H:%M:%SZ")
 
   curl --fail -s -k -u 'admin:'"${opensearch_admin_password}" \
     'https://opensearch.ops.simonklb.dev-ck8s.com/kubernetes/_search' \
     -H 'Content-Type: application/json' \
     -d '{
-      "sort":[{"@timestamp":{"order":"desc","unmapped_type":"boolean"}}]
+      "sort":[{"@timestamp":{"order":"desc","unmapped_type":"boolean"}}],
+      "query": {
+        "bool": {
+          "must": [],
+          "filter": [
+            {
+              "match_all": {}
+            },
+            {
+              "range": {
+                "@timestamp": {
+                  "gte": "'"${date_from}"'",
+                  "lte": "'"${date_now}"'",
+                  "format": "strict_date_optional_time"
+                }
+              }
+            }
+          ],
+          "should": [],
+          "must_not": []
+        }
+      }
     }' |
     jq -r '.hits.hits[] | [._source."@timestamp"[0:19], ._source.kubernetes.container_name, (._source.message // ._source.msg)] | join("¶")' |
     column -T 3 -t -s '¶'
 }
 
-echo -e "\033[1m== MANAGEMENT CLUSTER ==\033[0m"
-echo
-echo -e "\033[1mModule\033[0m"
-"${root}/bin/ck8s" ops kubectl sc get opensearch
-echo
-echo -e "\033[1mOpenSearch Pods\033[0m"
-"${root}/bin/ck8s" ops kubectl sc -n opensearch-system get po
-echo
-echo -e "\033[1mOpenSearch Ingress\033[0m"
-"${root}/bin/ck8s" ops kubectl sc -n opensearch-system get ingress -o custom-columns=NAME:.metadata.name,HOSTS:.spec.rules[].host
-echo
-echo -e "\033[1m== WORKLOAD CLUSTER ==\033[0m"
-echo
-echo -e "\033[1mModule\033[0m"
-"${root}/bin/ck8s" ops kubectl wc get fluentdforwarder
-echo
-echo -e "\033[1mEnvironmentConfigs\033[0m"
-"${root}/bin/ck8s" ops kubectl wc get environmentconfig
-echo
-echo -e "\033[1mFluentd DaemonSet env\033[0m"
-print_ds_env OUTPUT_HOSTS
-print_ds_env OUTPUT_USER
-print_ds_env OUTPUT_PASSWORD
-echo
-echo -e "\033[1mFluentd Pods\033[0m"
-"${root}/bin/ck8s" ops kubectl wc -n fluentd-system get po
-echo
-echo -e "\033[1m== OPENSEARCH LOGS ==\033[0m"
-search
+case "${1}" in
+mc | sc | all)
+  echo -e "\033[1m== MANAGEMENT CLUSTER ==\033[0m"
+  echo
+  echo -e "\033[1mModule\033[0m"
+  "${root}/bin/ck8s" ops kubectl sc get opensearch -o custom-columns='NAME:.metadata.name,SYNCED:status.conditions[?(@.type=="Synced")].status,READY:status.conditions[?(@.type=="Ready")].status'
+  echo
+  echo -e "\033[1mOpenSearch Pods\033[0m"
+  "${root}/bin/ck8s" ops kubectl sc -n opensearch-system get po -l '!job-name' | awk 'NF=3' | column -t
+  echo
+  echo -e "\033[1mOpenSearch Ingress\033[0m"
+  "${root}/bin/ck8s" ops kubectl sc -n opensearch-system get ingress -o custom-columns=NAME:.metadata.name,HOSTS:.spec.rules[].host
+  echo
+  ;;&
+wc | all)
+  echo -e "\033[1m== WORKLOAD CLUSTER ==\033[0m"
+  echo
+  echo -e "\033[1mModule\033[0m"
+  "${root}/bin/ck8s" ops kubectl wc get fluentdforwarder | awk 'NF=3' | column -t
+  echo
+  echo -e "\033[1mFluentd Pods\033[0m"
+  "${root}/bin/ck8s" ops kubectl wc -n fluentd-system get po | awk 'NF=3' | column -t
+  echo
+  echo -e "\033[1mEnvironmentConfigs\033[0m"
+  "${root}/bin/ck8s" ops kubectl wc get environmentconfig
+  echo
+  echo -e "\033[1mFluentd DaemonSet env\033[0m"
+  print_ds_env OUTPUT_HOSTS
+  print_ds_env OUTPUT_USER
+  print_ds_env OUTPUT_PASSWORD
+  echo
+  ;;&
+logs | all)
+  echo -e "\033[1m== OPENSEARCH LOGS (last 15 minutes) ==\033[0m"
+  echo
+  search
+  ;;
+esac
