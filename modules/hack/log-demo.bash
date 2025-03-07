89aaa02d26ae46ef24bad3e453e7147fe275d49f
Simon Kollberg <simonkollberg@gmail.com>
2025-03-07 17:14:56 +0100

    WIP

--- modules/hack/log-demo.bash
+++ modules/hack/log-demo.bash
@@ -51,7 +51,7 @@ install-ingress)
   ;;
 install-opensearch-deps)
   "${root}/bin/ck8s" ops helmfile sc apply -l netpol=service --include-transitive-needs
-  "${root}/bin/ck8s" ops helmfile sc apply -l name=crossplane-provider-configs --include-transitive-needs
+  "${root}/bin/ck8s" ops helmfile sc apply -l name=capability-log-write --include-transitive-needs
   "${root}/bin/ck8s" ops helmfile sc apply -l name=opensearch-secrets --include-transitive-needs
   "${root}/bin/ck8s" ops helmfile sc apply -l app=opensearch,policy=psp --include-transitive-needs
   ;;
@@ -68,7 +68,7 @@ uninstall-opensearch)
   "${root}/bin/ck8s" ops helmfile sc destroy -l name=module-opensearch
   ;;
 install-fluentd-deps)
-  "${root}/bin/ck8s" ops helmfile wc apply -l name=crossplane-provider-configs --include-transitive-needs
+  "${root}/bin/ck8s" ops helmfile wc apply -l name=capability-log-write --include-transitive-needs
   "${root}/bin/ck8s" ops helmfile wc apply -l app=fluentd,policy=netpol --include-transitive-needs
   "${root}/bin/ck8s" ops helmfile wc apply -l app=fluentd,policy=psp --include-transitive-needs
   "${root}/bin/ck8s" ops helmfile wc apply -l name=kube-prometheus-stack --include-transitive-needs
