bb0e85a64657134ccfca3538298a077a252588d0
Kim Alvefur <kim.alvefur@elastisys.com>
2025-03-11 10:55:09 +0100

    fix relative paths

--- tests/migration/v0.42/apply/80-apply.sh
+++ tests/migration/v0.42/apply/80-apply.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-ROOT="$(readlink -f "$(dirname "${0}")/../../../")"
+ROOT="$(readlink -f "$(dirname "${0}")/../../../../")"
 
 # shellcheck source=scripts/migration/lib.sh
 source "${ROOT}/scripts/migration/lib.sh"
