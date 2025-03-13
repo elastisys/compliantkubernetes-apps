0dc87f13c922597b2ba75cbd56bbd1d005ee36c8
Kim Alvefur <kim.alvefur@elasisys.com>
2025-03-13 13:58:49 +0100

    move setup and teardown to common file for general

---
+++ tests/integration/general/setup_suite.bash
@@ -0,0 +1,14 @@
+setup_suite() {
+  load "../../bats.lib.bash"
+  load_common "local-cluster.bash"
+
+  export CK8S_AUTO_APPROVE="true"
+
+  local_cluster.setup dev test.dev-ck8s.com
+  local_cluster.create sc single-node-cache
+}
+
+teardown_suite() {
+  local_cluster.delete sc
+  local_cluster.teardown
+}
