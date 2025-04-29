94e5f43147fa705c0110f5091286f35917fd2de0
Anders Bäckman <anders.backman@elastisys.com>
2025-04-29 17:19:25 +0200

    bin: fix ask_continue to not exit script

--- bin/common.bash
+++ bin/common.bash
@@ -105,7 +105,7 @@ ask_continue() {
   if [[ ! "${reply}" =~ ^[yY]$ ]]; then
     exit 0
   fi
-  exit 1
+  return
 }
 
 # Checks that all dependencies are available and critical ones for matching minor version.
