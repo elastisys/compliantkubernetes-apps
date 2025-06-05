2500acf681c0726e299eaa68912a499268e3d312
Mohamed Shafiq <mohamed.shafiq@elastisys.com>
2025-06-05 15:01:40 +0200

    removed execute from script

--- migration/v0.47/prepare/10-alertmanager.sh
+++ migration/v0.47/prepare/10-alertmanager.sh
@@ -7,12 +7,12 @@ ROOT="$(readlink -f "${HERE}/../../../")"
 source "${ROOT}/scripts/migration/lib.sh"
 
 if [[ "${CK8S_CLUSTER}" =~ ^(wc|both)$ ]]; then
-    log_info "Running alertmanager config migration in WC..."
-    yq_move wc '.user.alertmanager.ingress.enabled' '.prometheus.devAlertmanager.ingressEnabled'
-    yq_move wc '.user.alertmanager.resources' '.prometheus.alertmanagerSpec.resources'
-    yq_move wc '.user.alertmanager.tolerations' '.prometheus.alertmanagerSpec.tolerations'
-    yq_move wc '.user.alertmanager.affinity' '.prometheus.alertmanagerSpec.affinity'
-    yq_move wc '.user.alertmanager.topologySpreadConstraints' '.prometheus.alertmanagerSpec.topologySpreadConstraints'
-    yq_remove wc '.user.alertmanager'
-    yq_remove common '.user.alertmanager'
+  log_info "Running alertmanager config migration in WC..."
+  yq_move wc '.user.alertmanager.ingress.enabled' '.prometheus.devAlertmanager.ingressEnabled'
+  yq_move wc '.user.alertmanager.resources' '.prometheus.alertmanagerSpec.resources'
+  yq_move wc '.user.alertmanager.tolerations' '.prometheus.alertmanagerSpec.tolerations'
+  yq_move wc '.user.alertmanager.affinity' '.prometheus.alertmanagerSpec.affinity'
+  yq_move wc '.user.alertmanager.topologySpreadConstraints' '.prometheus.alertmanagerSpec.topologySpreadConstraints'
+  yq_remove wc '.user.alertmanager'
+  yq_remove common '.user.alertmanager'
 fi
