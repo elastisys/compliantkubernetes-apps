69c55dcb139c5c2afe9044f19b1afb29dd3fbf53
Mohamed Shafiq <mohamed.shafiq@elastisys.com>
2025-06-05 15:00:53 +0200

    removed execute from script

--- migration/v0.47/prepare/10-alertmanager.sh
+++ migration/v0.47/prepare/10-alertmanager.sh
@@ -6,27 +6,13 @@ ROOT="$(readlink -f "${HERE}/../../../")"
 # shellcheck source=scripts/migration/lib.sh
 source "${ROOT}/scripts/migration/lib.sh"
 
-run() {
-  case "${1:-}" in
-  execute)
-    if [[ "${CK8S_CLUSTER}" =~ ^(wc|both)$ ]]; then
-      log_info "Running alertmanager config migration in WC..."
-      config_load wc
-      yq_move wc '.user.alertmanager.ingress.enabled' '.prometheus.devAlertmanager.ingressEnabled'
-      yq_move wc '.user.alertmanager.resources' '.prometheus.alertmanagerSpec.resources'
-      yq_move wc '.user.alertmanager.tolerations' '.prometheus.alertmanagerSpec.tolerations'
-      yq_move wc '.user.alertmanager.affinity' '.prometheus.alertmanagerSpec.affinity'
-      yq_move wc '.user.alertmanager.topologySpreadConstraints' '.prometheus.alertmanagerSpec.topologySpreadConstraints'
-      yq_remove wc '.user.alertmanager'
-    fi
-    ;;
-  rollback)
-    log_warn "rollback not implemented"
-    ;;
-  *)
-    log_fatal "usage: \"${0}\" <execute|rollback>"
-    ;;
-  esac
-}
-
-run "${@}"
+if [[ "${CK8S_CLUSTER}" =~ ^(wc|both)$ ]]; then
+    log_info "Running alertmanager config migration in WC..."
+    yq_move wc '.user.alertmanager.ingress.enabled' '.prometheus.devAlertmanager.ingressEnabled'
+    yq_move wc '.user.alertmanager.resources' '.prometheus.alertmanagerSpec.resources'
+    yq_move wc '.user.alertmanager.tolerations' '.prometheus.alertmanagerSpec.tolerations'
+    yq_move wc '.user.alertmanager.affinity' '.prometheus.alertmanagerSpec.affinity'
+    yq_move wc '.user.alertmanager.topologySpreadConstraints' '.prometheus.alertmanagerSpec.topologySpreadConstraints'
+    yq_remove wc '.user.alertmanager'
+    yq_remove common '.user.alertmanager'
+fi
