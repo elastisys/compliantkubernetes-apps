a52f8cd024deb2fea2c21fa58f8a14ecd55f4ff5
Rareș Cosma <rares.cosma@elastisys.com>
2025-07-09 14:09:07 +0300

    cypress: use keyword arguments instead of positional for helpers

--- tests/end-to-end/prometheus/via-proxy.cy.js
+++ tests/end-to-end/prometheus/via-proxy.cy.js
@@ -1,19 +1,18 @@
-describe("prometheus", function() {
-  it("can be accessed via kubectl proxy", () => {
-    cy.visitProxied(
-      "wc",
-      "static-dev",
-      "true",
-      "http://127.0.0.1:8001/api/v1/namespaces/monitoring/services/kube-prometheus-stack-prometheus:9090/proxy/targets?pool=serviceMonitor%2Fmonitoring%2Fkube-prometheus-stack-apiserver%2F0"
-    )
+describe('prometheus', function () {
+  it('can be accessed via kubectl proxy', () => {
+    cy.visitProxied({
+      cluster: 'wc',
+      user: 'static-dev',
+      refresh: 'true',
+      url: 'http://127.0.0.1:8001/api/v1/namespaces/monitoring/services/kube-prometheus-stack-prometheus:9090/proxy/targets?pool=serviceMonitor%2Fmonitoring%2Fkube-prometheus-stack-apiserver%2F0',
+    })
 
-    cy.origin("http://127.0.0.1:8001", () => {
-      cy.contains("span", "up")
-        .should("exist")
+    cy.origin('http://127.0.0.1:8001', () => {
+      cy.contains('span', 'up').should('exist')
     })
   })
 
   after(() => {
-    cy.cleanupProxy("wc", "static-dev")
+    cy.cleanupProxy({ cluster: 'wc', user: 'static-dev' })
   })
 })
