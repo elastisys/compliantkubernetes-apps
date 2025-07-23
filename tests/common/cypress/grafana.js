b974b9a0118549ead56d909126ab4bbad69b122a
Rareș Cosma <rares.cosma@elastisys.com>
2025-07-23 09:07:42 +0200

    feat(tests): add grafana user promotion test
    Signed-off-by: Rareș Cosma <rares.cosma@elastisys.com>

--- tests/common/cypress/grafana.js
+++ tests/common/cypress/grafana.js
@@ -48,8 +48,8 @@ Cypress.Commands.add('testGrafanaDashboard', (ingress, dashboardName, expandRows
 })
 
 // Available as cy.grafanaDexStaticLogin("grafana.example.com")
-Cypress.Commands.add('grafanaDexStaticLogin', (ingress) => {
-  cy.session([ingress], () => {
+Cypress.Commands.add('grafanaDexStaticLogin', (ingress, cacheSession = true) => {
+  const login = function () {
     cy.visit(`https://${ingress}`)
 
     cy.contains('Sign in with dex').click()
@@ -57,11 +57,65 @@ Cypress.Commands.add('grafanaDexStaticLogin', (ingress) => {
     cy.dexStaticLogin()
 
     cy.getCookie('grafana_session_expiry').should('exist')
+  }
 
-    cy.contains('Home').should('exist')
-  })
+  if (cacheSession) {
+    cy.session([ingress], login)
+    cy.visit(`https://${ingress}`)
+  } else {
+    login()
+  }
+})
 
+// Available as cy.grafanaDexStaticLogin("grafana.example.com", "dev@example.com")
+Cypress.Commands.add('grafanaDexExtraStaticLogin', (ingress, staticUser) => {
   cy.visit(`https://${ingress}`)
 
-  cy.contains('Home').should('exist')
+  cy.contains('Sign in with dex').click()
+
+  cy.dexExtraStaticLogin(staticUser)
+
+  cy.getCookie('grafana_session_expiry').should('exist')
+})
+
+// Available as cy.grafanaSetRole(ingress, '.user.grafanaPassword', 'dev@example.com', 'Viewer')
+Cypress.Commands.add('grafanaSetRole', (ingress, adminPasswordKey, user, role) => {
+  // Log in as the grafana admin and change the role
+  cy.visit(`https://${ingress}/admin/users`)
+
+  cy.yqSecrets(adminPasswordKey).then((password) => {
+    cy.get('input[placeholder*="username"]').type('admin', { log: false })
+
+    cy.get('input[placeholder*="password"]').type(password, { log: false })
+
+    cy.get('button').contains('Log in').click()
+  })
+
+  cy.contains('Organization users').should('be.visible').click()
+
+  // remove TLD, form doesn't seem to like it
+  const searchString = user.substring(0, user.lastIndexOf('.'))
+  cy.get('input[placeholder*="Search user by login"]')
+    .should('not.be.disabled')
+    .type(`${searchString}{enter}`)
+
+  cy.get('[aria-label="Role"]').as('role').focus()
+  cy.get('@role').should('not.be.disabled').type(`${role}{enter}`, { force: true })
+
+  cy.contains('Organization user updated').should('be.visible')
+})
+
+// Available as cy.grafanaCheckRole(ingress, 'dev@example.com', 'Admin')
+Cypress.Commands.add('grafanaCheckRole', (ingress, user, role) => {
+  cy.intercept('/api/user/orgs').as('userOrgs')
+
+  if (user === 'admin@example.com') {
+    cy.grafanaDexStaticLogin(`${ingress}/profile`, false)
+  } else {
+    cy.grafanaDexExtraStaticLogin(`${ingress}/profile`, user)
+  }
+
+  cy.wait(['@userOrgs'])
+
+  cy.get('[data-testid="data-testid-user-orgs-table"]').contains(role).should('exist')
 })
---
+++ tests/end-to-end/grafana/admin-promotion-ops.gen.bats
@@ -0,0 +1,24 @@
+#!/usr/bin/env bats
+
+setup_file() {
+  load "../../bats.lib.bash"
+
+  cypress_setup "${ROOT}/tests/end-to-end/grafana/admin-promotion-ops.cy.js"
+}
+
+setup() {
+  load "../../bats.lib.bash"
+  load_assert
+}
+
+teardown_file() {
+  cypress_teardown
+}
+
+@test "ops grafana user promotion admin demotes admin@example.com to Viewer" {
+  cypress_test "ops grafana user promotion admin demotes admin@example.com to Viewer"
+}
+
+@test "ops grafana user promotion admin promotes admin@example.com to Admin" {
+  cypress_test "ops grafana user promotion admin promotes admin@example.com to Admin"
+}
