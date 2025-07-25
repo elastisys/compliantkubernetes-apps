ef7f11126547f6b4c1995622f04f221ab6ed3ea7
Rareș Cosma <rares.cosma@elastisys.com>
2025-07-25 10:35:15 +0200

    tests: make refresh=true the default for visitProxied

--- tests/end-to-end/alertmanager/via-proxy.cy.js
+++ tests/end-to-end/alertmanager/via-proxy.cy.js
@@ -6,8 +6,6 @@ describe('alertmanager', function () {
       cluster: 'wc',
       user: 'static-dev',
       url: 'http://127.0.0.1:8001/api/v1/namespaces/alertmanager/services/alertmanager-operated:9093/proxy/',
-      refresh: true,
-      checkAdmin: true,
     })
 
     cy.origin('http://127.0.0.1:8001', () => {
--- tests/end-to-end/prometheus/via-proxy.cy.js
+++ tests/end-to-end/prometheus/via-proxy.cy.js
@@ -4,8 +4,6 @@ describe('prometheus', function () {
       cluster: 'wc',
       user: 'static-dev',
       url: 'http://127.0.0.1:8001/api/v1/namespaces/monitoring/services/kube-prometheus-stack-prometheus:9090/proxy/targets?pool=serviceMonitor%2Fmonitoring%2Fkube-prometheus-stack-apiserver%2F0',
-      refresh: true,
-      checkAdmin: true,
     })
 
     cy.origin('http://127.0.0.1:8001', () => {
