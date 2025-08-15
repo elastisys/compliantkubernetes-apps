9808064e6a35ebc9ace04cee31c1f532f599244d
Rareș Cosma <rares.cosma@elastisys.com>
2025-08-15 09:22:15 +0200

    velero: add usage for all common functions

--- tests/end-to-end/velero/common.bash
+++ tests/end-to-end/velero/common.bash
@@ -1,31 +1,39 @@
+# Usage: velero_backups_spec <cluster>
 velero_backups_spec() {
   ck8s ops velero "${1}" backup create --from-schedule velero-daily-backup -o yaml 2>/dev/null | yq '.spec | del(.excludedNamespaces)'
 }
 
+# Usage: velero_backups_excluded_ns <cluster>
 velero_backups_excluded_ns() {
   ck8s ops velero "${1}" backup create --from-schedule velero-daily-backup -o yaml 2>/dev/null | yq '.spec.excludedNamespaces'
 }
 
+# Usage: velero_expected_spec <cluster>
 velero_expected_spec() {
   yq 'del(.excludedNamespaces)' <"${BATS_TEST_DIRNAME}/resources/backup-spec-${1}.yaml"
 }
 
+# Usage: velero_expected_excluded_ns <cluster>
 velero_expected_excluded_ns() {
   yq '.excludedNamespaces' <"${BATS_TEST_DIRNAME}/resources/backup-spec-${1}.yaml"
 }
 
+# Usage: velero_backup_create <cluster> <backup_name>
 velero_backup_create() {
   ck8s ops velero "${1}" backup create "${2}" --from-schedule velero-daily-backup --wait
 }
 
+# Usage: velero_backup_get_phase <cluster> <backup_name>
 velero_backup_get_phase() {
   ck8s ops velero "${1}" backup get "${2}" -o json 2>/dev/null | jq -r .status.phase
 }
 
+# Usage: velero_restore_create <cluster> <backup_name>
 velero_restore_create() {
   ck8s ops velero "${1}" restore create "${2}" --from-backup "${3}" --wait
 }
 
+# Usage: velero_restore_get_phase <cluster> <backup_name>
 velero_restore_get_phase() {
   ck8s ops velero "${1}" restore get "${2}" -o json 2>/dev/null | jq -r .status.phase
 }
