2e474313cf2b70b3e3d789069f4073ad87c6aa52
Anders Bäckman <anders.backman@elastisys.com>
2025-08-21 12:59:49 +0200

    bin: install-requirements path fix

--- bin/install-requirements.bash
+++ bin/install-requirements.bash
@@ -2,6 +2,8 @@
 
 set -euo pipefail
 
+root_path="$(readlink -f "$(dirname "${0}")/../")"
+
 export ANSIBLE_STDOUT_CALLBACK=yaml
 
 echo "This script will install all requirements"
@@ -9,11 +11,11 @@ echo "Enter your password to start the install"
 
 if [ "${#}" -ge 1 ] && [ "${1}" = "--user" ]; then
   export CK8S_INSTALL_PATH="${CK8S_INSTALL_PATH:-"${HOME}/bin"}"
-  ansible-playbook -e 'ansible_python_interpreter=/usr/bin/python3' --ask-become-pass roles/get-requirements.yaml
+  ansible-playbook -e 'ansible_python_interpreter=/usr/bin/python3' --ask-become-pass "${root_path}/roles/get-requirements.yaml"
 else
   if [ "${#}" -ge 1 ] && [ "${1}" = "--no-pass" ]; then
-    ansible-playbook -e 'ansible_python_interpreter=/usr/bin/python3' --become --become-user root roles/get-requirements.yaml
+    ansible-playbook -e 'ansible_python_interpreter=/usr/bin/python3' --become --become-user root "${root_path}/roles/get-requirements.yaml"
   else
-    ansible-playbook -e 'ansible_python_interpreter=/usr/bin/python3' --ask-become-pass --become --become-user root roles/get-requirements.yaml
+    ansible-playbook -e 'ansible_python_interpreter=/usr/bin/python3' --ask-become-pass --become --become-user root "${root_path}/roles/get-requirements.yaml"
   fi
 fi
