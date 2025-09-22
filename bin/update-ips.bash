ab41724bd6a7d2f4eb998fda5b91eeb36d948c2c
Rareș Cosma <rares.cosma@elastisys.com>
2025-09-22 17:55:33 +0300

    fix: ignore extracted ips from upcloud state if they come out empty

--- bin/update-ips.bash
+++ bin/update-ips.bash
@@ -631,11 +631,15 @@ allow_ingress() {
     sc_private_address=$(jq -r '.resources[].instances[].attributes.nodes | select( . != null) | .[].networks[].ip_addresses[] | select( .listen == false) | .address' "${CK8S_CONFIG_PATH}"/sc-config/terraform.tfstate)
     wc_private_address=$(jq -r '.resources[].instances[].attributes.nodes | select( . != null) | .[].networks[].ip_addresses[] | select( .listen == false) | .address' "${CK8S_CONFIG_PATH}"/wc-config/terraform.tfstate)
 
-    readarray -t sc_array <<<"${sc_private_address[@]}"
-    readarray -t wc_array <<<"${wc_private_address[@]}"
+    if [[ -n "$sc_private_address" ]]; then
+      readarray -t sc_array <<<"${sc_private_address[@]}"
+      append_ips "${config["override_common"]}" '.networkPolicies.global.scIngress.ips' "${sc_array[@]}"
+    fi
 
-    append_ips "${config["override_common"]}" '.networkPolicies.global.scIngress.ips' "${sc_array[@]}"
-    append_ips "${config["override_common"]}" '.networkPolicies.global.wcIngress.ips' "${wc_array[@]}"
+    if [[ -n "$wc_private_address" ]]; then
+      readarray -t wc_array <<<"${wc_private_address[@]}"
+      append_ips "${config["override_common"]}" '.networkPolicies.global.wcIngress.ips' "${wc_array[@]}"
+    fi
   fi
 }
 
