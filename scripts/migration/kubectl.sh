500d76042cb9fb5da906ca180137198ff7ec51ac
Staffan Linnaeus <staffan.linnaeus@elastisys.com>
2025-09-24 15:46:46 +0200

    feat: dry-run helpers for migration scripts

--- scripts/migration/kubectl.sh
+++ scripts/migration/kubectl.sh
@@ -5,10 +5,28 @@ kubectl_do() {
     log_fatal "usage: kubectl_do <sc|wc> <args>..."
   fi
 
+  local -a args=()
+
+  if [[ "${2}" =~ ^- ]]; then
+    log_fatal "Second argument must be a kubectl operation, not an option"
+  fi
+
+  if [[ "${CK8S_DRY_RUN_INSTALL}" == "true" ]]; then
+    local command="${2}"
+    # List of common commands that generally DO NOT support --dry-run=server
+    case "${command}" in
+    get | describe | logs | api-resources | api-versions | cluster-info | config | kustomize | proxy | version) ;;
+    *)
+      args+=("--dry-run=server")
+      ;;
+    esac
+  fi
+
+  args+=("${@:2}")
   if [ "${CONFIG["${1}-kubeconfig"]}" = "encrypted" ]; then
     kubectl --kubeconfig <(sops --decrypt "${CK8S_CONFIG_PATH}/.state/kube_config_${1}.yaml") "${@:2}"
   else
-    kubectl --kubeconfig "${CK8S_CONFIG_PATH}/.state/kube_config_${1}.yaml" "${@:2}"
+    kubectl --kubeconfig "${CK8S_CONFIG_PATH}/.state/kube_config_${1}.yaml" "${args[@]}"
   fi
 }
 
