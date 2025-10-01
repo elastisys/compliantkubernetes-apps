502e7f8de6cd50bdca0356fe1d3a5e9f8cd621e3
Rareș Cosma <rares.cosma@elastisys.com>
2025-10-01 08:45:55 +0300

    tests: split promotion tests + add dependent configuration checks

--- tests/common/bats/grafana.bash
+++ tests/common/bats/grafana.bash
@@ -4,7 +4,9 @@
 # - _grafana.curl [method] [resource/path] [json data] <args...>  - curl helper function (not intended to be used outside of this file)
 # - grafana.load_env [slug]                                       - setup endpoint and auth info; slug is one of ['ops_admin', 'user_admin', 'ops_static', 'user_static']
 # - grafana.get_dashboards                                        - get all defined dashboards
-# - grafana.get_actual_user                                       - get currently logged in user
+# - grafana.load_actual_user                                      - load currently logged in user info into the environment
+# - grafana.change_role [role]                                    - change user role with assertion
+# - grafana.check_role [role]                                     - check user role
 
 _grafana.curl() {
   local method="${1}"
@@ -84,6 +86,20 @@ grafana.get_dashboards() {
   _grafana.curl GET "apis/dashboard.grafana.app/v1beta1/namespaces/default/dashboards?limit=200" ""
 }
 
-grafana.get_actual_user() {
-  _grafana.curl GET "api/user" ""
+grafana.load_actual_user() {
+  local -r actual_user="$(_grafana.curl GET "api/user" "")"
+  grafana_org_id="$(echo "$actual_user" | jq -r '.orgId')"
+  grafana_user_id="$(echo "$actual_user" | jq -r '.id')"
+}
+
+grafana.change_role() {
+  role="${1:-}"
+  run _grafana.curl PATCH "api/orgs/${grafana_org_id}/users/${grafana_user_id}" "{\"role\":\"${role}\"}"
+  assert_output --partial "Organization user updated"
+}
+
+grafana.check_role() {
+  role="${1:-}"
+  run bats_pipe _grafana.curl GET "api/users/${grafana_user_id}/orgs" "" \| jq -r '.[0].role'
+  assert_line "${role}"
 }
