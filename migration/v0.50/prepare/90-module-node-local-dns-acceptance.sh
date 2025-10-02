10d32b5f09c19f23c2829520a1f5dba97085f778
Simon Kollberg <simonkollberg@gmail.com>
2025-10-02 17:18:30 +0200

    apps: Replace node-local-dns with Module

---
+++ migration/v0.50/prepare/90-module-node-local-dns-acceptance.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+HERE="$(dirname "$(readlink -f "${0}")")"
+ROOT="$(readlink -f "${HERE}/../../../")"
+
+# shellcheck source=scripts/migration/lib.sh
+source "${ROOT}/scripts/migration/lib.sh"
+
+module_acceptance "${CK8S_CLUSTER}" node-local-dns "${HERE}/module-accepted-changes/node-local-dns"
