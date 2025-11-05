a5c69b5f194b3200a7a35ef6bba8e35cb48adacf
HaoruiPeng <haorui.peng@elastisys.com>
2025-11-05 14:58:15 +0100

    end-to-end

---
+++ pipeline/test/end-to-end.bash
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+: "${CK8S_CONFIG_PATH:?Missing CK8S_CONFIG_PATH}"
+
+set -u -o pipefail
+
+root=$(dirname "$(dirname "$(dirname "$(readlink -f "$0")")")")
+log_dir=${root}/logs
+
+if [ ! -d "${log_dir}" ]; then
+  mkdir -p "${log_dir}"
+fi
+
+cd "${root}/tests" || exit
+
+mapfile -t dirs < <(find end-to-end -mindepth 1 -maxdepth 1 -type d ! -name '.*' -printf '%f\n')
+
+for dir in "${dirs[@]}"; do
+  log_file="${log_dir}/end-to-end-${dir}.log"
+  if ! make run-end-to-end/"${dir}" 2>&1 | tee "${log_file}"; then
+    echo "Test for ${dir} failed. See ${log_file} for details."
+  else
+    rm "${log_file}"
+  fi
+done
