f62d879c1eedf443204e45a7c9495eabffaf0dfd
Simon Kollberg <simonkollberg@gmail.com>
2025-11-10 14:09:15 +0100

    module_maintenance_test

--- migration/v0.50/prepare/90-module-node-local-dns-maintenance.sh
+++ migration/v0.50/prepare/90-module-node-local-dns-maintenance.sh
@@ -6,4 +6,4 @@ ROOT="$(readlink -f "${HERE}/../../../")"
 # shellcheck source=scripts/migration/lib.sh
 source "${ROOT}/scripts/migration/lib.sh"
 
-module_acceptance "${CK8S_CLUSTER}" app=node-local-dns "${HERE}/module-accepted-changes/node-local-dns"
+module_maintenance_test "${CK8S_CLUSTER}" app=node-local-dns "${HERE}/module-accepted-changes/node-local-dns"
